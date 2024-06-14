576303de7dbcbc11f580abd7da9ae966f9947fc1
KIM SOOBIN <xpsxm225@naver.com>
2024-06-14 14:28:32 +0900

    [React] props children속성

--- langaunges/JavaScript/React/01-starting-project/src/App.jsx
+++ langaunges/JavaScript/React/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx'
+import TabButton from './components/TabButton/TabButton.jsx'
 
 import { CORE_CONCEPTS } from './data.js';
 
@@ -34,6 +35,15 @@ function App() {
               ))
             }
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
