3f957a676b5fcaa7c59f50eb0db79bf5732aa014
KIM SOOBIN <xpsxm225@naver.com>
2024-06-15 09:48:48 +0900

    [React] 이벤트 처리

--- langaunges/JavaScript/React/01-starting-project/src/components/TabButton/TabButton.jsx
+++ langaunges/JavaScript/React/01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,6 +1,10 @@
 export default function TabButton(props) {
+    function handleClick(){
+        console.log('Hello World!');
+    }
     return (<li>
-        <button>
+        {/* onClick prop 전달 */}
+        <button onClick={handleClick}>
             {props.children}
         </button>
     </li>)
