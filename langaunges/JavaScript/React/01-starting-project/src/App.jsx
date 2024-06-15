1dd1190c0f2e87ba96ee0d63a666b77feebd1390
KIM SOOBIN <xpsxm225@naver.com>
2024-06-15 13:01:36 +0900

    [React] 상태. useState Hook

--- langaunges/JavaScript/React/01-starting-project/src/App.jsx
+++ langaunges/JavaScript/React/01-starting-project/src/App.jsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept/CoreConcept.jsx'
 import TabButton from './components/TabButton/TabButton.jsx'
@@ -5,6 +7,13 @@ import TabButton from './components/TabButton/TabButton.jsx'
 import { CORE_CONCEPTS } from './data.js';
 
 function App() {
+  // let tabContent = 'Please click a button';
+  let [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  function handleSelect(selectedButton){
+    // selectedButton -> 'components;, 'jsx', 'props', 'state'
+    // tabContent = selectedButton; 
+    setSelectedTopic(selectedButton);
+  }
   return (
     <div>
       <Header />
@@ -22,11 +31,17 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            {/* 
+              onSelect은 정의된 클릭이벤트가 아니고, 사용자 정의 
+              () => {}는, 전개하면
+               function () => 표현의 익명함수
+            */}
+            <TabButton onSelect={()=>handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={()=>handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={()=>handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={()=>handleSelect('state')}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
