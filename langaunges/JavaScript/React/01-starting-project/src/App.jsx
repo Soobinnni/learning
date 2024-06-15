7f86932765f765b393cda064f3a7c60a6745d598
KIM SOOBIN <xpsxm225@naver.com>
2024-06-15 09:55:40 +0900

    [React] CoreConcept 주석 지움

--- langaunges/JavaScript/React/01-starting-project/src/App.jsx
+++ langaunges/JavaScript/React/01-starting-project/src/App.jsx
@@ -12,23 +12,6 @@ function App() {
         <section id='core-concepts'>
           <h2>Core Concepts</h2>
           <ul>
-            {/* 
-            ====================first====================
-            <CoreConcept 
-              img={componentsImg} 
-              title="Components"
-              description="The core UI building block"  
-            />
-            <CoreConcept />
-            <CoreConcept />
-            <CoreConcept /> 
-
-            ====================second====================
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} /> 
-            */}
             {
               CORE_CONCEPTS.map((data, index) => (
                 <CoreConcept key={index} {...data} />
