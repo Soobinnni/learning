4a19e1968d71e4a05a656e7524297fdd152bd878
KIM SOOBIN <xpsxm225@naver.com>
2024-06-18 16:04:37 +0900

    [React] Wrapper Component와 스프레드 연산자 활용(html 속성)

--- langaunges/JavaScript/React/01-starting-project/src/components/Tab/TabButton.jsx
+++ langaunges/JavaScript/React/01-starting-project/src/components/Tab/TabButton.jsx
@@ -1,7 +1,11 @@
-export default function TabButton({children, onSelect, isSelected}) {
+export default function TabButton({children, isSelected, ...props}) {
     return (<li>
-        {/* onClick prop 전달 */}
-        <button className={isSelected?"active":undefined} onClick={onSelect}>
+        {/* 
+            onClick prop 전달 
+            스프레드 연산자 사용하기 위해 onClick으로 props 명 변경
+        */}
+
+        <button className={isSelected?"active":undefined} {...props}>
             {children}
         </button>
     </li>)
