c28352ca8642e54585cdf06ae5c84ffe4f82bedc
KIM SOOBIN <xpsxm225@naver.com>
2024-06-19 15:47:38 +0900

    [React] useState이용하여 버튼과 내용 바꾸기

--- langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/App.jsx
+++ langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,13 +1,13 @@
 import Player from "./components/Player"
+
+
 function App() {
-
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-        <Player name="Player1" symbol="X" />
-        <Player name="Player2" symbol="O" />
+          <Player name="Player1" symbol="X"/>
+          <Player name="Player2" symbol="O" />
         </ol>
         GAME BOARD
       </div>
--- langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,9 +1,27 @@
-export default function Player({name, symbol}){
-    return <li>
-            <span className="plyaer">
-                <span className="player-name">{name}</span>
+import { useState } from 'react';
+export default function Player({ name, symbol }) {
+    const [isEditing, setIsEditing] = useState(false);
+    let buttonContent = "Edit";
+    let playerName = <span className="player-name">{name}</span>;
+
+    // function handlingEditClick() {
+    //     setIsEditing(!isEditing);
+    // }
+
+    if (isEditing) {
+        buttonContent = "Save";
+        playerName = <input type="text" required />;
+    } else {
+        buttonContent = "Edit";
+        playerName = <span className="player-name">{name}</span>;
+    }
+    return (
+        <li>
+            <span className="player">
+                {playerName}
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={()=>{setIsEditing(!isEditing)}}>{buttonContent}</button>
         </li>
+    )
 }
