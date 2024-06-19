6edef9d86d04abda9e8df96f55f17e71b8b9e8e2
KIM SOOBIN <xpsxm225@naver.com>
2024-06-19 16:30:24 +0900

    [React] event객체를 활용하여 name save기능 추가

--- langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react';
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
     const [isEditing, setIsEditing] = useState(false);
+    const [playerName, setPlayerName] = useState(initialName);
 
-    const defaultNameSpan = <span className="player-name">{name}</span>;
-    const nameInput = <input type="text" required value={name}/>;
+    const defaultNameSpan = <span className="player-name">{playerName}</span>;
+    const nameInput = <input type="text" required defaultValue={playerName} onChange={(event)=>{
+        setPlayerName(name=>event.target.value);
+    }}/>;
 
     return (
         <li>
@@ -13,7 +16,9 @@ export default function Player({ name, symbol }) {
                 <span className="player-symbol">{symbol}</span>
             </span>
             {/* <button onClick={() => { setIsEditing((editing)=>{ return !editing}) }}>{isEditing ? 'Save' : 'Edit'}</button> */}
-            <button onClick={() => { setIsEditing(editing=>!editing) }}>{isEditing ? 'Save' : 'Edit'}</button>
+            <button onClick={() => { 
+                setIsEditing(editing=>!editing);
+             }}>{isEditing ? 'Save' : 'Edit'}</button>
         </li>
     )
 }
