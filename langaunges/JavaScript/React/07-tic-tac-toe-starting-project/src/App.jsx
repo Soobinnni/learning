4bc403f6a2314e08185851ff897706a43224e4cd
KIM SOOBIN <xpsxm225@naver.com>
2024-06-21 17:06:58 +0900

    [React] 주제: 승리 component 띄우기
    App.jsx에 필요한 것: 게임 종료 확인, 승리자 결정

--- langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/App.jsx
+++ langaunges/JavaScript/React/07-tic-tac-toe-starting-project/src/App.jsx
@@ -4,6 +4,9 @@ import Log from "./components/Log.jsx"
 
 import { useState } from "react"
 
+import { WINNING_COMBINATIONS } from "./winning-combination.js"
+
+// ================== HELPER , CONST ==================
 function deriveActivePlayer(gameTurns){
   let curActivePlayer = 'X';
 
@@ -15,9 +18,31 @@ function deriveActivePlayer(gameTurns){
 
   return curActivePlayer;
 }
+
+const initalGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+// ================== ================== ==================
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
+  const [hasWinner, setHasWinner] = useState(false);
+
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  // GameBoad.jsx component에서 가져옴(승리 조건 확인을 위해 board를 살펴볼 필요가 있으므로)
+  let gameBoard = initalGameBoard;
+  let winner = null;
+
+  // gameBoard가 저장해 놓은 turns에 의해서 update
+  for(const turn of gameTurns){
+      const {square, player} = turn;
+      const {row, col} = square;
+
+      gameBoard[row][col]=player;
+  }
 
   function handleSelectSquare(rowIndex, colIndex){
       setGameTurns(preTurns=>{
@@ -30,6 +55,19 @@ function App() {
         return updatedTurns;
       });
   }
+
+  for (const combination of WINNING_COMBINATIONS){
+    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+    if(firstSquareSymbol&&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol){
+        winner = firstSquareSymbol;
+    }
+  }
+
   return (
     <main>
       <div id="game-container">
@@ -37,7 +75,8 @@ function App() {
           <Player initialName="Player1" symbol="X" isActive={activePlayer === "X"}/>
           <Player initialName="Player2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+        { winner && <p>You won, {winner}!</p>}
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
       </div>
       <Log turns = {gameTurns}/>
     </main>
