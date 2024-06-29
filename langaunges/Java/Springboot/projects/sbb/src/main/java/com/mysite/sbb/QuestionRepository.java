643706f7a943cd9ca0723b99779c3f8b3a86ccf5
KIM SOOBIN <xpsxm225@naver.com>
2024-06-29 16:48:01 +0900

    [Spring Boot] Repository 수정-어노테이션 제거

--- langaunges/Java/Springboot/projects/sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
+++ langaunges/Java/Springboot/projects/sbb/src/main/java/com/mysite/sbb/QuestionRepository.java
@@ -1,9 +1,9 @@
 package com.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+//  JpaRepository는 Spring Data JPA에서 제공하는 인터페이스로, 스프링 부트 애플리케이션에서 자동으로 빈으로 등록.
+// 따라서 개발자가 별도로 @Repository 어노테이션을 명시적으로 추가할 필요가 없다.
 public interface QuestionRepository extends JpaRepository<Question, Long>{
 
 }
