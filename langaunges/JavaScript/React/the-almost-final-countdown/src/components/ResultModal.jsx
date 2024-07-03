ddeff48f0d81663f4ccffe20887ac09d8e4471af
KIM SOOBIN <xpsxm225@naver.com>
2024-07-03 17:59:55 +0900

    [React] createPortal을 이용해서 React 트리와 DOM 트리 구조 분리

--- langaunges/JavaScript/React/the-almost-final-countdown/src/components/ResultModal.jsx
+++ langaunges/JavaScript/React/the-almost-final-countdown/src/components/ResultModal.jsx
@@ -1,6 +1,7 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
 
-const ResultModal = forwardRef(function ResultModal({remainingTime, targetTime, onReset}, ref){
+const ResultModal = forwardRef(({remainingTime, targetTime, onReset}, ref) => {
     const dialog = useRef();
 
     const userLost = remainingTime <= 0;
@@ -14,26 +15,28 @@ const ResultModal = forwardRef(function ResultModal({remainingTime, targetTime, 
             }
         }
     });
-    return (
-        <dialog 
-            className="result-modal" 
-            ref = {dialog}
-            onClose={onReset}
-            >
-            {
-                userLost &&
-                <h2>Your lost</h2>
-            }
-            {
-                !userLost && <h2>Your Score: {score}</h2>
-            }
-            <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
+    return createPortal (
+        (
+            <dialog 
+                className="result-modal" 
+                ref = {dialog}
+                onClose={onReset}
+                >
+                {
+                    userLost &&
+                    <h2>Your lost</h2>
+                }
+                {
+                    !userLost && <h2>Your Score: {score}</h2>
+                }
+                <p>The target time was <strong>{targetTime} seconds.</strong></p>
+                <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
 
-            <form action="dialog" onSubmit={onReset}> 
-                <button>Close</button>
-            </form>
-        </dialog>
+                <form action="dialog" onSubmit={onReset}> 
+                    <button>Close</button>
+                </form>
+            </dialog>
+        ), document.getElementById('modal')
     )
 })
 
