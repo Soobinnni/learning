4f7a7b24a8dcbbfb421c6098aca33d58a6c40c6a
KIM SOOBIN <xpsxm225@naver.com>
2024-07-03 12:08:57 +0900

    [React] fowardRef를 활용하여 자식 컴포넌트에 ref 전달, 부모DOM에 자식 노드 노출.

--- langaunges/JavaScript/React/the-almost-final-countdown/src/components/TimerChallenge.jsx
+++ langaunges/JavaScript/React/the-almost-final-countdown/src/components/TimerChallenge.jsx
@@ -1,50 +1,55 @@
 import { useRef, useState } from "react";
+import ResultModal from './ResultModal.jsx';
 
-export default function TimerChallenge({title, targetTime}) {
+export default function TimerChallenge({ title, targetTime }) {
     /*
      * 도전을 시작하면, 흐른 시간을 유추해서 멈춤을 누르는 컴포넌트
      */
 
     const timer = useRef();
+    const dialog = useRef();
 
     const [timeStarted, setTimeStarted] = useState(false);
     const [timeExpired, setTimeExpired] = useState(false);
 
-    function handleStart () {
-        timer.current = 
+    function handleStart() {
+        timer.current =
             setTimeout(() => {
                 setTimeExpired(true);
+                dialog.current.showModal();
             }, targetTime * 1000);
         setTimeStarted(true);
     }
-    function handleStop(){
-        // 어떻게 handleStart 내 setTimeout 함수에 접근하느냐??
-        // ---> ref, 참조를 이용한다.
+    function handleStop() {
         clearTimeout(timer.current);
     }
     return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {
-                timeExpired && <p>You lost!</p>
-            }
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 && 's'}
-            </p>
-            <p>
-                <button
-                    onClick={timeStarted?handleStop:handleStart}
-                >
-                    {timeStarted?'Stop':'Start'} Challenge
-                </button>
-            </p>
-            <p className={timeStarted?'active':undefined}>
-                {
-                    timeStarted?
-                        'Time is running...' :
-                        'Timer inactive'
-                }
-            </p>
-        </section>
+        <>
+            <ResultModal
+                targetTime={targetTime}
+                result="lost"
+                ref = {dialog}
+                />
+            <section className="challenge">
+                <h2>{title}</h2>
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 && 's'}
+                </p>
+                <p>
+                    <button
+                        onClick={timeStarted ? handleStop : handleStart}
+                    >
+                        {timeStarted ? 'Stop' : 'Start'} Challenge
+                    </button>
+                </p>
+                <p className={timeStarted ? 'active' : undefined}>
+                    {
+                        timeStarted ?
+                            'Time is running...' :
+                            'Timer inactive'
+                    }
+                </p>
+            </section>
+        </>
     )
 }
