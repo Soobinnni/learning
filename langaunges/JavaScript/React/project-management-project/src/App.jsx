4e8626eb88636d276893bf4eac8fb97a02bd5650
KIM SOOBIN <xpsxm225@naver.com>
2024-07-03 20:19:26 +0900

    [React] 기본 컴포넌트 구성

--- langaunges/JavaScript/React/project-management-project/src/App.jsx
+++ langaunges/JavaScript/React/project-management-project/src/App.jsx
@@ -1,8 +1,13 @@
+import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
+import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSideBar/>
+      <NoProjectSelected/>
+    </main>
   );
 }
 
