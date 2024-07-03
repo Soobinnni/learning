7e3a03817d5729f8ace49cbbd6e9226be4001b5d
KIM SOOBIN <xpsxm225@naver.com>
2024-07-03 20:43:50 +0900

    [React] new project 버튼에 따라 동적으로 content 불러오기

--- langaunges/JavaScript/React/project-management-project/src/App.jsx
+++ langaunges/JavaScript/React/project-management-project/src/App.jsx
@@ -1,12 +1,34 @@
 import ProjectsSideBar from "./components/ProjectsSideBar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(preProjectsState => {
+      return {
+        ...preProjectsState,
+        selectedProjectId: null // null이면, 프로젝트를 새로 만들겠다는 의미.
+      }
+    });
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId===null){
+    content = <NewProject/>
+  } else if(projectsState.selectedProjectId===undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar/>
-      <NoProjectSelected/>
+      <ProjectsSideBar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
