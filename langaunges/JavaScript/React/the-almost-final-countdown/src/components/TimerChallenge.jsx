093663215ebbc25bf9052e1dfb27ee8ee484eaf1
KIM SOOBIN <xpsxm225@naver.com>
2024-07-03 11:17:42 +0900

    [React] useRef hook의 리렌더링 안정성, 인스턴스별 고유성, 상태와의 일관성, 컴포넌트 수명 내 유지 특성 활용(timer)

--- langaunges/JavaScript/React/the-almost-final-countdown/src/components/TimerChallenge.jsx
+++ langaunges/JavaScript/React/the-almost-final-countdown/src/components/TimerChallenge.jsx
@@ -1,22 +1,26 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function TimerChallenge({title, targetTime}) {
     /*
      * 도전을 시작하면, 흐른 시간을 유추해서 멈춤을 누르는 컴포넌트
      */
 
+    const timer = useRef();
 
     const [timeStarted, setTimeStarted] = useState(false);
     const [timeExpired, setTimeExpired] = useState(false);
+
     function handleStart () {
-        /*
-         *  setTimeout 첫 번째 인자: time out 후 호출될 함수
-         *              두 번째 인자: time 
-         */
-        setTimeout(() => {
-            setTimeExpired(true);
-        }, targetTime * 1000);
+        timer.current = 
+            setTimeout(() => {
+                setTimeExpired(true);
+            }, targetTime * 1000);
         setTimeStarted(true);
+    }
+    function handleStop(){
+        // 어떻게 handleStart 내 setTimeout 함수에 접근하느냐??
+        // ---> ref, 참조를 이용한다.
+        clearTimeout(timer.current);
     }
     return (
         <section className="challenge">
@@ -29,7 +33,7 @@ export default function TimerChallenge({title, targetTime}) {
             </p>
             <p>
                 <button
-                    onClick={handleStart}
+                    onClick={timeStarted?handleStop:handleStart}
                 >
                     {timeStarted?'Stop':'Start'} Challenge
                 </button>
