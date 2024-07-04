436cc8e08f94d01d5609d344b18590dfb0b12527
KIM SOOBIN <xpsxm225@naver.com>
2024-07-04 21:40:37 +0900

    [React] Context에 state, state update함수 공동 사용하기

--- langaunges/JavaScript/React/elegant-context/src/store/shopping-cart-context.jsx
+++ langaunges/JavaScript/React/elegant-context/src/store/shopping-cart-context.jsx
@@ -2,7 +2,8 @@ import { createContext } from "react";
 
 // Context 생성
 export const CartContext = createContext({
-    items: []
+    items: [],
+    addItemToCart: () => {}
 });
 
 // CartContext 값에 접근하는 컴포넌트를 CartContext.Provider로 묶기
