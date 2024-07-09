964db04d868e0f351c0580610921d6c8b6609f1d
KIM SOOBIN <xpsxm225@naver.com>
2024-07-09 15:54:41 +0900

    [React] fetch 데이터 현재 장소 별로 정렬, 요청 error 처리.

--- langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
+++ langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
@@ -1,27 +1,55 @@
 import { useState } from 'react';
 import Places from './Places.jsx';
 import { useEffect } from 'react';
+import Error from './Error.jsx';
+import { sortPlacesByDistance } from '../loc.js';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
-  // 비동기 업데이트에 시간이 걸리므로, 
-  // 빈배열에 set하는 것으로 요청 데이터를 담아야 한다.
-  // set함수는 무한 루프를 야기할 수 있으니 useEffect에 정의.
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true);
-      const response = await fetch('http://localhost:3000/places');
-      const resData = await response.json();
 
-      setAvailablePlaces(resData.places);
-      setIsFetching(false);
+      try {
+        const response = await fetch('http://localhost:3000/places');
+        const resData = await response.json();
+
+        // 에러 다루기
+        if (!response.ok) { // !(200, 300) == 400, 500
+          throw new Error('Failed to fetch places');
+        }
+
+        navigator.geolocation.getCurrentPosition((position)=>{
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places, 
+            position.coords.latitude,
+            position.coords.longitude
+          )
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        // error 객체를 state에 저장한다. setError(error);
+        // message가 대체로 있지만, 없을 경우 대체 텍스트를 지정하여 저장한다.
+        setError({
+          message : error.message || "Could not fetch places, please try again later."
+        });
+        setIsFetching(false);
+        
+      }
     }
-
     fetchPlaces();
   }, []);
 
+  if(error){
+    return <Error 
+              title="An error occurred!"
+              message={error.message}
+          />
+  }
   return (
     <Places
       title="Available Places"
