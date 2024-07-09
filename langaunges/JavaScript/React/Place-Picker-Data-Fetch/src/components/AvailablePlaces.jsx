4d9433247893b1709522fd75e1f4429c01569d64
KIM SOOBIN <xpsxm225@naver.com>
2024-07-09 15:28:32 +0900

    [React] 로딩 시 대체 텍스트 요소 보이기

--- langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
+++ langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
@@ -4,16 +4,19 @@ import { useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   // 비동기 업데이트에 시간이 걸리므로, 
   // 빈배열에 set하는 것으로 요청 데이터를 담아야 한다.
   // set함수는 무한 루프를 야기할 수 있으니 useEffect에 정의.
   useEffect(() => {
     async function fetchPlaces() {
-    const response = await fetch('http://localhost:3000/places');
-    const resData = await response.json();
+      setIsFetching(true);
+      const response = await fetch('http://localhost:3000/places');
+      const resData = await response.json();
 
-    setAvailablePlaces(resData)
+      setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
 
     fetchPlaces();
@@ -23,6 +26,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
