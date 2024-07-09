ba26bd72b11c6aabb390b266b635c4550edc0cd1
KIM SOOBIN <xpsxm225@naver.com>
2024-07-09 14:57:52 +0900

    [React] 이미지 서버 요청

--- langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
+++ langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react';
 import Places from './Places.jsx';
+import { useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+
+  // 비동기 업데이트에 시간이 걸리므로, 
+  // 빈배열에 set하는 것으로 요청 데이터를 담아야 한다.
+  // set함수는 무한 루프를 야기할 수 있으니 useEffect에 정의.
+  useEffect(() => {
+    async function fetchPlaces() {
+    const response = await fetch('http://localhost:3000/places');
+    const resData = await response.json();
+
+    setAvailablePlaces(resData)
+    }
+
+    fetchPlaces();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
