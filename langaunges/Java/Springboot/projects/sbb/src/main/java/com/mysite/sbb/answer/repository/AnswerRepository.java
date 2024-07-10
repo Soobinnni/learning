ede5a801690a2704583c6e6a991051b36c46231f
KIM SOOBIN <xpsxm225@naver.com>
2024-07-10 15:32:41 +0900

    폴더명 수정

--- langaunges/Java/Springboot/projects/sbb/src/main/java/com/mysite/sbb/answer/repository/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mysite.sbb.answer.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.mysite.sbb.answer.entity.Answer;
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-
-}
--- langaunges/Java/Springboot/projects/sbb/src/main/java/com/mysite/sbb/answer/service/AnswerService.java
+++
@@ -1,7 +0,0 @@
-package com.mysite.sbb.answer.service;
-
-import com.mysite.sbb.question.dto.QuestionDTO;
-
-public interface AnswerService {
-    public void create(QuestionDTO questionDTO, String content) throws Exception;
-}
