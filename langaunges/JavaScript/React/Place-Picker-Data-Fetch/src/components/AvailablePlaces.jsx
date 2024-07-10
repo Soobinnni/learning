43039e6f0181962ceaf003d30abea972f6df509c
KIM SOOBIN <xpsxm225@naver.com>
2024-07-10 15:19:31 +0900

    [React] 정렬 처리를 위해 새 비동기 함수 작성.

--- langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
+++ langaunges/JavaScript/React/Place-Picker-Data-Fetch/src/components/AvailablePlaces.jsx
@@ -1,49 +1,49 @@
-import { useState } from 'react';
+import { useFetch } from '../hooks/useFetch.js';
+
 import Places from './Places.jsx';
-import { useEffect } from 'react';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
 
+async function fetchSortedPlaces() {
+
+  /* fetchSortedPlaces 정의
+   * 1. fetch한 데이터를 set할 뿐만 아니라 AvailablePlaces 컴포넌트는
+        사용자 위치 기반의 거리순 정렬이 필요함
+     2. 따라서, useFetch에 함수 인자를 전달할 때 함수에 정렬 로직도 포함하기 위해
+        새 함수(fetchSortedPlaces)를 정의
+     3. places를 비동기적으로 받아오고, Promise를 반환.
+   */
+
+  const places = await fetchAvailablePlaces();
+
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      )
+
+      resolve(sortedPlaces);
+    });
+
+  });
+}
+
 
 export default function AvailablePlaces({ onSelectPlace }) {
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [error, setError] = useState();
+  const {
+    error,
+    fetchedData: availablePlaces,
+    isFetching
+  } = useFetch(fetchSortedPlaces, []);
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
-
-      try {
-        const places = await fetchAvailablePlaces();
-        navigator.geolocation.getCurrentPosition((position)=>{
-          const sortedPlaces = sortPlacesByDistance(
-            places, 
-            position.coords.latitude,
-            position.coords.longitude
-          )
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        // error 객체를 state에 저장한다. setError(error);
-        // message가 대체로 있지만, 없을 경우 대체 텍스트를 지정하여 저장한다.
-        setError({
-          message : error.message || "Could not fetch places, please try again later."
-        });
-        setIsFetching(false);
-        
-      }
-    }
-    fetchPlaces();
-  }, []);
-
-  if(error){
-    return <Error 
-              title="An error occurred!"
-              message={error.message}
-          />
+  if (error) {
+    return <Error
+      title="An error occurred!"
+      message={error.message}
+    />
   }
   return (
     <Places
