91fd4f62ea404664da9d79e0d333627d7c4be802
KIM SOOBIN <xpsxm225@naver.com>
2024-07-10 21:21:27 +0900

    [React] context api를 통해 전역 상태 관리하기, reducer로 복잡한 상태 변화 함수 정의하기.

---
+++ languages/JavaScript/React/Food-Order/src/store/CartContext.jsx
@@ -0,0 +1,76 @@
+import { createContext, useReducer } from "react";
+
+const CartContext = createContext({
+    items: [],
+    addItem: (item) => { },
+    removeItem: (id) => { }
+});
+
+function cartReducer(state, action) {
+    const { type, item, id } = action;
+
+    const updatedItems = [...state.items];
+
+    // ======================================================================
+    if (type === 'ADD_ITEM') {
+        const existingCartItemIndex = state.items.findIndex((target) =>
+            target.id === item.id
+        )
+
+        if (existingCartItemIndex > -1) {
+            const existingItme = state.items[existingCartItemIndex]
+            const updatedItem = {
+                ...existingItme,
+                quantity: existingItme.quantity + 1
+            }
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            updatedItems.push({ ...item, quantity: 1 });
+        }
+
+        return { ...state, items: updatedItems };
+    }
+    // ======================================================================
+    if (type === 'REMOVE_ITEM') {
+        const existingCartItemIndex = state.items.findIndex((target) =>
+            target.id === id
+        )
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        if (existingCartItem.quantity > 1) {
+            updatedItems.slice(existingCartItemIndex, 1);
+        } else {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity - 1
+            }
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        return { ...state, items: updatedItems };
+    }
+
+    return state;
+}
+
+export function CartContextProvider({ children }) {
+    const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+
+    function addItem(item){ dispatchCartAction({
+        type: 'ADD_ITEM',
+        item
+    })}
+    function removeItem(id){ dispatchCartAction({
+        type: 'REMOVE_ITEM',
+        id
+    })}
+
+    const cartContext = {
+        items: cart.items,
+        addItem,
+        removeItem
+    }
+    console.log(cartContext)
+    return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
+}
+
+export default CartContext;
