a9d94a5af03891f7525f910e23f6085345af8421
KIM SOOBIN <xpsxm225@naver.com>
2024-07-10 23:49:11 +0900

    [React] 입력 양식과 주문 양식을 서버에 저장 요청

--- languages/JavaScript/React/Food-Order/src/components/Checkout.jsx
+++ languages/JavaScript/React/Food-Order/src/components/Checkout.jsx
@@ -13,17 +13,39 @@ export default function Checkout() {
     const cartTotal = cartCtx.items.reduce((totalPrice, item) => {
         return totalPrice + item.quantity * item.price
     }, 0)
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        
+        const fd = new FormData(event.target);
+        const customerData = Object.fromEntries(fd.entries());
+
+        console.log(customerData)
+        // post request
+        fetch('http://localhost:3000/orders',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                order: {
+                    items: cartCtx.items,
+                    customer: customerData
+                }
+            })
+        })
+    }
     return (
         <Modal 
             open={userProgressCtx.progress==='checkout'} 
             onClose={userProgressCtx.progress==='checkout'?userProgressCtx.hideCheckout:null}
         >
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h2>Checkout</h2>
                 <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
-                <Input label='Full Name' type='text' id="full-name" />
-                <Input label='E-Mail Address' type='email id="email"' />
-                <Input label='Street' type='text' id="full-name" />
+                <Input label='Full Name' type='text' id="name" />
+                <Input label='E-Mail Address' type='email' id="email" />
+                <Input label='Street' type='text' id="street" />
                 <div className="control-row">
                     <Input label='Postal Code' type='text' id="postal-code" />
                     <Input label='City' type='text' id="city" />
