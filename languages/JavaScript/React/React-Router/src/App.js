875224cc97339ff761acb90a93b94f3fb0b8be12
KIM SOOBIN <xpsxm225@naver.com>
2024-07-13 14:18:57 +0900

    [React] Router 정의.

--- languages/JavaScript/React/React-Router/src/App.js
+++ languages/JavaScript/React/React-Router/src/App.js
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import HomePage from "./pages/Home.js";
+import ProductsPage from "./pages/ProductsPage.js";
+
 function App() {
-  return <div></div>;
+  const router = createBrowserRouter([
+    { path: '/', element: <HomePage /> },
+    { path: '/products', element: <ProductsPage /> },
+  ])
+
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
---
+++ languages/JavaScript/React/React-Router/src/pages/ProductsPage.js
@@ -0,0 +1,3 @@
+export default function ProductsPage(){
+    return <h3>The Products Page</h3>
+}
