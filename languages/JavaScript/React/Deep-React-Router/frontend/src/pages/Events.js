d020ca2f99fccbe672fe6506fde08e79beaa0e50
KIM SOOBIN <xpsxm225@naver.com>
2024-07-14 03:17:25 +0900

    [React] 속도가 다른 다수의 요청이 있을 때 defer의 작동 조절하기(await 키워드)

--- languages/JavaScript/React/Deep-React-Router/frontend/src/pages/Events.js
+++ languages/JavaScript/React/Deep-React-Router/frontend/src/pages/Events.js
@@ -4,14 +4,8 @@ import EventsList from '../components/EventsList';
 import { Suspense } from 'react';
 
 function EventsPage() {
-    // const data = useLoaderData();
-    // const events = data.events;
     const { events } = useLoaderData();
 
-    // return <EventsList events={events} />
-
-    //  Suspense 컴포넌트는 다른 데이터가 도착하길 기다리는 동안에 폴백을 보여주는
-    // 특정한 상황에서 사용한다.
     return (
         <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
             <Await resolve={events}>
@@ -37,18 +31,12 @@ async function loadEvents() {
             }
         );
     } else {
-        // return response; loader함수 내에서는 자동으로 해결했지만 defer를 거치므로, 직접 parse해야 함
         const resData = await response.json();
         return resData.events;
     }
 }
 export async function loader() {
-    // defer함수에 해당 페이지에서 오갈 수 있는 http요청을 담는다.
-    // defer함수에는 미래에 리졸빙될 값이 있는 Promise객체들을 인자로 전달한다.
-    // 이 defer함수를 loader의 리턴값으로 리턴한다.
     return defer({
-        events: loadEvents() // loadEvents()의 리턴값을 events 키에 저장.
-        // lodEvents()의 리턴값은 Promise객체
+        events: loadEvents()
     })
-    // 연기된 데이터를 사용하려는 컴포넌트로 ...
 }
--- languages/JavaScript/React/Deep-React-Router/frontend/src/pages/EventsDetail.js
+++ languages/JavaScript/React/Deep-React-Router/frontend/src/pages/EventsDetail.js
@@ -1,37 +1,74 @@
-import { json, redirect, useRouteLoaderData } from "react-router-dom";
+import { defer, json, redirect, useRouteLoaderData, Await } from "react-router-dom";
 
 import EventItem from '../components/EventItem.js'
+import EventsList from "../components/EventsList.js";
+import { Suspense } from "react";
 
 const EventDetailPage = () => {
-    const data = useRouteLoaderData('event-detail');
-  
-    return <EventItem event={data.event} />;
+    const { event, events } = useRouteLoaderData('event-detail');
+
+    return <>
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={event}>
+                {(loadedEvent) => <EventItem event={loadedEvent} />}
+            </Await>
+        </Suspense>
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    </>;
 };
 
 export default EventDetailPage;
 
-export const loader = async({request, params}) => {
-    const id = params.eventId;
-    const response = await fetch(`http://localhost:8080/events/${id}`);
-    if(!response.ok){
+async function loadEvents() {
+    const response = await fetch('http://localhost:8080/events');
+
+    if (!response.ok) {
         throw json(
-            {message: 'Could not fetch details for selected event'}, 
-            {status: response.status}
-        )
+            { message: 'Could not fetch events.' },
+            {
+                status: 500,
+            }
+        );
     } else {
-        return response;
+        const resData = await response.json();
+        return resData.events;
     }
 }
 
-export const action = async({request, params}) => {
-    const response = await fetch(`http://localhost:8080/events/${params.eventId}`,{
+async function loadEvent(id) {
+    const response = await fetch(`http://localhost:8080/events/${id}`);
+    if (!response.ok) {
+        throw json(
+            { message: 'Could not fetch details for selected event' },
+            { status: response.status }
+        )
+    } else {
+        const resData = await response.json();
+        return resData.event;
+    }
+}
+
+export const loader = async ({ params }) => {
+    return defer({
+        event: await loadEvent(params.eventId),
+        events: loadEvents(),
+    })
+}
+
+
+export const action = async ({ request, params }) => {
+    const response = await fetch(`http://localhost:8080/events/${params.eventId}`, {
         // method: 'DELETE'
         //       submit(null, {method:'delete'}) 에서 가져옴
         method: request.method
     });
 
-    if(!response.ok){
-        throw json({message:'Could not delete event.'},{status:500})
+    if (!response.ok) {
+        throw json({ message: 'Could not delete event.' }, { status: 500 })
     }
 
     return redirect('/events');
