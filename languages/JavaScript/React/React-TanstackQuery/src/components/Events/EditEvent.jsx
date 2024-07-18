69ff4fe23a5bcc07cd8abf35e2e65deabf928a0e
KIM SOOBIN <xpsxm225@naver.com>
2024-07-18 13:18:48 +0900

    [React] Tanstack Query 마무리

--- languages/JavaScript/React/React-TanstackQuery/src/components/Events/EditEvent.jsx
+++ languages/JavaScript/React/React-TanstackQuery/src/components/Events/EditEvent.jsx
@@ -1,33 +1,39 @@
-import { Link, useNavigate, useParams } from 'react-router-dom';
+import { Link, redirect, useNavigate, useParams, useSubmit, useNavigation } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useMutation, useQuery } from '@tanstack/react-query';
+import { /*useMutation,*/ useQuery } from '@tanstack/react-query';
 import { queryClient, fetchEvent, updateEvent } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function EditEvent() {
   /**
-   * Update에서 낙관적 업데이트 하기.
-   * - 백그라운드에서 쿼리 무효화 및 활성 쿼리 재요청과 무관하게,
-   *   수정 내용을 UI에만 바로 반영하여 낙관적 업데이트를 한다.
-   * - 역시 마찬가지로, 실패하더라도 낙관적 업데이트 후 롤백한다.
+   * loaderData로 fetchQuery의 데이터를 가져오지만, useQuery는 유지하는 것이 좋다.
+   *  (이미 캐시된 상태이므로 useLoaderData를 호출할 필요 없음)
+   * 왜냐하면 fetchQuery의 결과 역시 캐시되고 이를 useQuery가 다시 실행되면
+   * 캐시된 결과를 가져오는 캐싱 메커니즘이 그대로 적용되기 때문이다.
+   * 또한, 창을 나갔다가 다시 들어오는 백그라운드 트리거의 효과도 필요하기 때문에 유지한다.
    * 
+   * 대신, 미리 데이터를 로드하므로 pending상태에 관한 내용은 삭제한다.
+   * 또한 error상태를 load함수가 다루므로 error상태 관리 내용도 삭제할 수 있다.
    */
-
-
   const navigate = useNavigate();
   const { id } = useParams();
 
+
   // query
   const {
-    data, state, isError, error
+    data, isError, error
   } = useQuery({
     queryKey: ['events', { id }],
-    queryFn: ({ signal }) => fetchEvent({ signal, id })
+    queryFn: ({ signal }) => fetchEvent({ signal, id }),
+
+    // React Router와 Tanstack Query의 쿼리 중복 요청을 방지
+    // 캐시된 데이터가 10초 미만인 경우 내부적으로 다시 가져오지 않는다.
+    staleTime: 10000
   });
 
-  // mutation
+  /* mutation
   const { mutate } = useMutation({
     mutationFn: updateEvent,
     onMutate: async (data) => {
@@ -50,12 +56,12 @@ export default function EditEvent() {
       queryClient.invalidateQueries(['events', { id }]);
     }
   })
+    */
 
+  const submit = useSubmit();
+  const {state} = useNavigation();
   function handleSubmit(formData) {
-    // onSuccess를 여기서 다루지 않음.
-    // isPending을 다룰 수 있는 여러 방법
-    mutate({ id, event: formData });
-    navigate('../');
+    submit(formData, { method: 'PUT' });
   }
 
   function handleClose() {
@@ -105,4 +111,30 @@ export default function EditEvent() {
 
   return <Modal onClose={handleClose}>{content}</Modal>;
 
+}
+
+export function loader({ params }) {
+  const { id } = params;
+  return queryClient.fetchQuery({
+    queryKey: ['events', { id }],
+    queryFn: ({ signal }) => fetchEvent({ signal, id })
+  });
+}
+
+export async function action({ request, params }) {
+  /**
+   * 대신 낙관적 업데이트를 수행하지 못하게 됨.
+   * 실행하기 위해선 submit메소드에서 자체 로직을 작성해야만 함.
+   */
+  const formData = await request.formData();
+  const updatedEventData = Object.fromEntries(formData);
+
+  const { id } = params;
+
+  await updateEvent({ id, event: updatedEventData });
+
+  // 이후 queryClient로 해당 mutation과 관련된 query를 무효화, 다시 가져오도록 한다.
+  await queryClient.invalidateQueries(['events', { id }]);
+
+  return redirect('../')
 }
--- languages/JavaScript/React/React-TanstackQuery/src/components/Header.jsx
+++ languages/JavaScript/React/React-TanstackQuery/src/components/Header.jsx
@@ -1,7 +1,10 @@
+import { useIsFetching } from "@tanstack/react-query";
+
 export default function Header({ children }) {
+  const isFetching = useIsFetching(); // 쿼리가 데이터를 가져오지 않으면 0, 가져오면 0초과로 늘어남
   return (
     <>
-      <div id="main-header-loading"></div>
+      <div id="main-header-loading">{isFetching>0 && <progress/>}</div>
       <header id="main-header">
         <div id="header-title">
           <h1>React Events</h1>
