0cc357f33e5d1cc94e36a4712c109e9eb8b3f31f
KIM SOOBIN <xpsxm225@naver.com>
2024-07-19 23:23:35 +0900

    [Spring Boot] springboot-developer Project Init

---
+++ languages/Java/Springboot/projects/springboot-developer/src/test/java/kim/soov/springbootdeveloper/SpringbootDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.soov.springbootdeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeveloperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
