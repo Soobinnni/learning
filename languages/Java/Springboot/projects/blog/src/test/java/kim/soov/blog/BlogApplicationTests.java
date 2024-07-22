fe3544610ecd4fcdca8c260be65f7cd276104539
KIM SOOBIN <xpsxm225@naver.com>
2024-07-22 23:01:24 +0900

    [Spring Boot] Blog Project Init

---
+++ languages/Java/Springboot/projects/blog/src/test/java/kim/soov/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package kim.soov.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
