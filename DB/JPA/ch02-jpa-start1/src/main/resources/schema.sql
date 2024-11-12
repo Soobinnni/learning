445b1a0cf4752cd6dc8a427eafa7eec11bcf6398
KIM SOOBIN <xpsxm225@naver.com>
2024-11-12 14:33:12 +0900

    [JPA] 버전 업에 의한 코드 수정

---
+++ DB/JPA/ch02-jpa-start1/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE MEMBER (
+                        id VARCHAR(255) NOT NULL,
+                        name VARCHAR(255),
+                        age INTEGER NOT NULL,
+                        PRIMARY KEY(id)
+);
