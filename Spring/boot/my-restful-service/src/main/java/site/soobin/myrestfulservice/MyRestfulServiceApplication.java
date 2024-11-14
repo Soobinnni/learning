e20f81b70f9f28d4d31e72d85f7f38dcaff83b88
KIM SOOBIN <xpsxm225@naver.com>
2024-11-14 20:36:11 +0900

    [SpringBoot] 빈 목록 확인
    org.springframework.context.ApplicationContext의 getBeanDefinitionNames를 통해 등록된 빈의 목록을 문자열로 확인할 수 있다.

--- Spring/boot/my-restful-service/src/main/java/site/soobin/myrestfulservice/MyRestfulServiceApplication.java
+++ Spring/boot/my-restful-service/src/main/java/site/soobin/myrestfulservice/MyRestfulServiceApplication.java
@@ -2,11 +2,18 @@ package site.soobin.myrestfulservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MyRestfulServiceApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MyRestfulServiceApplication.class, args);
+    ApplicationContext ac = SpringApplication.run(MyRestfulServiceApplication.class, args);
+
+    // 애플리케이션 컨텍스트를 통해 현재 스프링 컨텍스트에 등록된 빈들의 목록을 확인할 수 있다.
+    String[] allBeansNames = ac.getBeanDefinitionNames();
+    for (String beanName : allBeansNames) {
+      System.out.println(beanName);
+    }
   }
 }
