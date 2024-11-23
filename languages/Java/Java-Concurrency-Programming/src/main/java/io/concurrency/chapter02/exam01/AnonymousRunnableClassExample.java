48fe6f7eae0f60e77c05274ab90e62d98f3d11f6
KIM SOOBIN <xpsxm225@naver.com>
2024-11-23 13:21:17 +0900

    [Java-Concurrency] Thread, Runnable 익명 클래스 또는 람다식 구현 예시

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam01/AnonymousRunnableClassExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam01/AnonymousRunnableClassExample.java
@@ -3,6 +3,7 @@ package io.concurrency.chapter02.exam01;
 public class AnonymousRunnableClassExample {
     public static void main(String[] args) {
 
+        // 익명 Runnable 구현 -> Thread에 전달
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam01/AnonymousThreadClassExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam01/AnonymousThreadClassExample.java
@@ -2,7 +2,8 @@ package io.concurrency.chapter02.exam01;
 
 public class AnonymousThreadClassExample {
     public static void main(String[] args) {
-
+        // 익명 클래스로 생성
+        // 재사용하지 않을 때 익명 Thread 객체 생성
         Thread thread = new Thread() {
             @Override
             public void run() {
