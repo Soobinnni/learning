a493454ec78104d82ff81019c10fc6a36e791e9f
KIM SOOBIN <xpsxm225@naver.com>
2024-11-23 13:15:23 +0900

    [Java-Concurrency] Runnable 인터페이스를 구현->Thread 생성자에 전달하여 Thread 생성

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam01/ImplementRunnableExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam01/ImplementRunnableExample.java
@@ -4,13 +4,14 @@ public class ImplementRunnableExample {
     public static void main(String[] args) {
 
         MyRunnable task = new MyRunnable();
+        // 2. Thread 생성자에 전달하여 Thread 생성
         Thread thread = new Thread(task);
         thread.start();
     }
 }
 
 class MyRunnable implements Runnable{
-
+    // 1. Runnable 인터페이스를 구현
     @Override
     public void run() {
         System.out.println(Thread.currentThread().getName() + ": 스레드 실행 중");
