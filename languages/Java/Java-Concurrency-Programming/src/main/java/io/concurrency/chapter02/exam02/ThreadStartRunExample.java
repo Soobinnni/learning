cd3297fed22a05494ab76b779a3b3fce8655bdd1
KIM SOOBIN <xpsxm225@naver.com>
2024-11-24 11:15:58 +0900

    [Java-Concurrency] Thread start로 인한 스레드 생성 및 커널 스레드 매핑, run은 jvm이 자동으로 호출하고 직접 호출하지 않는다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam02/ThreadStartRunExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter02/exam02/ThreadStartRunExample.java
@@ -12,6 +12,8 @@ public class ThreadStartRunExample {
         });
 
         thread.start();
+        // run은 직접 호출하지 않는다. 새로운 스레드와 스택이 생기지 않고 해당 메서드를 호출한 스택(메인 스레드)
+        // 에 쌓이고, 실행될 뿐이다.
 //        thread.run();
 //        myRunnable.run();
 
@@ -21,6 +23,8 @@ public class ThreadStartRunExample {
 
         @Override
         public void run() {
+            // 정의된 run은 jvm이 호출하므로 자동으로 호출된다.
+            // 직접 호출하지 않는다.
             System.out.println(Thread.currentThread().getName() + ": 스레드 실행 중...");
         }
     }
