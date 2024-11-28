036eddb1bcf793941949c32417336ee4df42d938
KIM SOOBIN <xpsxm225@naver.com>
2024-11-28 21:13:13 +0900

    [Java-Concurrency] ThreadLocal의 사용
    - ThreadLocal은 Thread와 ThreadLocalMap을 연결하여 스레드 전용 저장소를 구현
    - 초기값을 설정할 수 있음
    - set할 시에 ThreadLocalMap이 생성(내부 동작 참고)
    - ThreadLocalMap은 내부적으로 ThreadLocal을 key로 사용하여 value에 접근

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter04/exam05/ThreadLocalExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter04/exam05/ThreadLocalExample.java
@@ -1,27 +1,45 @@
 package io.concurrency.chapter04.exam05;
 
 public class ThreadLocalExample {
-    // ThreadLocal 변수 생성. 초기값은 null
-    private static ThreadLocal<String> threadLocal = ThreadLocal.withInitial(() -> "Hello World");
-//    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+  // ThreadLocal 변수 생성. 초기값은 null
+  private static ThreadLocal<String> threadLocal = ThreadLocal.withInitial(() -> "Hello World");
 
-    public static void main(String[] args) {
+  //    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
 
-        // 첫 번째 스레드: ThreadLocal 값을 설정하고 출력
-        Thread thread1 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
-            threadLocal.set("스레드 1의 값");
-            System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
-        }, "Thread-1");
+  public static void main(String[] args) {
 
-        // 두 번째 스레드: ThreadLocal 값을 설정하고 출력
-        Thread thread2 = new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
-            threadLocal.set("스레드 2의 값");
-            System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
-        }, "Thread-2");
+    // 첫 번째 스레드: ThreadLocal 값을 설정하고 출력
+    Thread thread1 =
+        new Thread(
+            () -> {
+              System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
 
-        thread1.start();
-        thread2.start();
-    }
+              // 참고: set 내부 동작
+              // public void set(T value) {
+              //        Thread t = Thread.currentThread();
+              //        ThreadLocalMap map = getMap(t);
+              //        if (map != null) {
+              //            map.set(this, value);
+              //        } else {
+              //            createMap(t, value);
+              //        }
+              //    }
+              threadLocal.set("스레드 1의 값");
+              System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
+            },
+            "Thread-1");
+
+    // 두 번째 스레드: ThreadLocal 값을 설정하고 출력
+    Thread thread2 =
+        new Thread(
+            () -> {
+              System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
+              threadLocal.set("스레드 2의 값");
+              System.out.println(Thread.currentThread().getName() + ": " + threadLocal.get());
+            },
+            "Thread-2");
+
+    thread1.start();
+    thread2.start();
+  }
 }
