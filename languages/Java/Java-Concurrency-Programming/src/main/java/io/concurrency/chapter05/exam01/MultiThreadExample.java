24cddda3acb15b463fece0df3c0e41b01d39e788
KIM SOOBIN <xpsxm225@naver.com>
2024-11-30 15:46:57 +0900

    [Java-Concurrency-Programming] 멀티스레드의 특징과 유의점
    - 멀티스레드 환경에서 한 스레드의 오류가 발생하더라도 전체 프로그래밍의 종류나 다른 스레드의 영향으로 이어지지 않는다.
    - 싱글스레드 환경에서 스레드의 오류는 프로그램 종료로 이어진다.
    - 멀티스레드 환경에서 조심해야할 것은 동시성의 문제다. 공유 자원이 있고 동시에 여러 스레드가 읽고/쓰기를 진행한다면 문제가 발생할 수 있다.
    - 개발자는 적절한 동기화 기법으로 해당 이슈를 막아야 한다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter05/exam01/MultiThreadExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter05/exam01/MultiThreadExample.java
@@ -1,50 +1,59 @@
 package io.concurrency.chapter05.exam01;
 
 public class MultiThreadExample {
-    private static int sum = 0;
-    private static final Object lock = new Object();
+  private static final Object lock = new Object();
+  private static int sum = 0;
 
-    public static void main(String[] args) {
-        long start = System.currentTimeMillis();
+  public static void main(String[] args) {
+    long start = System.currentTimeMillis();
 
-        Thread thread1 = new Thread(() -> {
-            for (int i = 1; i <= 500; i++) {
+    Thread thread1 =
+        new Thread(
+            () -> {
+              for (int i = 1; i <= 500; i++) {
                 synchronized (lock) {
-                    sum += i;
+                  // 동시성 문제가 일어날 수 있는 부분에 synchronized 를 통해
+                  // lock을 획득하고 먼저 작업을 수행한다. sum 변수가 공유 자원이기 때문이다.
+                  // 이 구문이 없다면 동시성 문제가 발생한다.
+                  sum += i;
                 }
                 try {
-                    Thread.sleep(1);
+                  Thread.sleep(1);
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                  e.printStackTrace();
                 }
-            }
-        });
+              }
+            });
 
-        Thread thread2 = new Thread(() -> {
-            for (int i = 501; i <= 1000; i++) {
+    Thread thread2 =
+        new Thread(
+            () -> {
+              for (int i = 501; i <= 1000; i++) {
                 synchronized (lock) {
-                    sum += i;
+                  sum += i;
                 }
                 try {
-                    Thread.sleep(1);
-                    throw new RuntimeException("error");
+                  Thread.sleep(1);
+                  // thread가 오류가 발생하더라도 다른 스레드에 영향을 끼치지 않으며
+                  // 전체 애플리케이션의 종료로 이어지지 않는다.
+                  throw new RuntimeException("error");
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                  e.printStackTrace();
                 }
-            }
-        });
+              }
+            });
 
-        thread1.start();
-        thread2.start();
+    thread1.start();
+    thread2.start();
 
-        try {
-            thread1.join();
-            thread2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("합계: " + sum);
-        System.out.println("멀티 스레드 처리 시간: " + (System.currentTimeMillis() - start) + "ms");
+    try {
+      thread1.join();
+      thread2.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+
+    System.out.println("합계: " + sum);
+    System.out.println("멀티 스레드 처리 시간: " + (System.currentTimeMillis() - start) + "ms");
+  }
 }
