9352c8536f09a3b3ece4d811df10ca0c13a27f0c
KIM SOOBIN <xpsxm225@naver.com>
2024-11-30 18:31:04 +0900

    [Java-Concurrency] 안전한 스레드 구성
    1. 지역변수를 사용한다.
        - 각 스레드는 별도의 저장공간(Stack)을 가지므로 개별적으로 저장되는 지역변수를 사용하여 안전한 코드를 구성한다.
    2. 객체를 형성하더라도 지역변수로 구성한다.
        - 참조값을 가진 객체는 스레드에 안전하지 않을 수 있으나 1과 같은 이유에서 지역변수로서 사용하면 안전하다.
    3. 파라미터로 전달되어 다수의 스레드가 공유하는 참조 객체는 위험하다
    4. 불변의 객체로 만들어(final 키워드, setter 없음) 필드를 변경할 요소들을 원천 차단한다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter05/exam04/ImmutableExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter05/exam04/ImmutableExample.java
@@ -1,41 +1,47 @@
 package io.concurrency.chapter05.exam04;
 
-public class ImmutableExample  implements Runnable{
-    private ImmutablePerson person;
+public class ImmutableExample implements Runnable {
+  private ImmutablePerson person;
 
-    public ImmutableExample(ImmutablePerson person) {
-        this.person = person;
+  public ImmutableExample(ImmutablePerson person) {
+    this.person = person;
+  }
+
+  public static void main(String[] args) {
+    // 필드 자체가 변경될 모든 요소를 원천 차단
+    ImmutablePerson person = new ImmutablePerson("홍길동", 25);
+
+    for (int i = 0; i < 10; i++) {
+      new Thread(new ImmutableExample(person)).start();
     }
+  }
 
-    @Override
-    public void run() {
-        System.out.println(Thread.currentThread().getName() + " - 이름: " + person.getName() + ", 나이: " + person.getAge());
-    }
-
-    public static void main(String[] args) {
-
-        ImmutablePerson person = new ImmutablePerson("홍길동", 25);
-
-        for (int i = 0; i < 10; i++) {
-            new Thread(new ImmutableExample(person)).start();
-        }
-    }
+  @Override
+  public void run() {
+    System.out.println(
+        Thread.currentThread().getName()
+            + " - 이름: "
+            + person.getName()
+            + ", 나이: "
+            + person.getAge());
+  }
 }
 
 final class ImmutablePerson {
-    private final String name;
-    private final int age;
+  // 변경 불가능한 클래스로 만들어 동시성 문제를 없앤다. -> final키워드, setter 없음
+  private final String name;
+  private final int age;
 
-    public ImmutablePerson(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
+  public ImmutablePerson(String name, int age) {
+    this.name = name;
+    this.age = age;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public int getAge() {
-        return age;
-    }
+  public int getAge() {
+    return age;
+  }
 }
--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter05/exam04/ThreadSafeLocalReferenceObjectExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter05/exam04/ThreadSafeLocalReferenceObjectExample.java
@@ -1,48 +1,54 @@
 package io.concurrency.chapter05.exam04;
 
 public class ThreadSafeLocalReferenceObjectExample {
-    class LocalObject {
-        private int value;
+  public static void main(String[] args) {
+    ThreadSafeLocalReferenceObjectExample example = new ThreadSafeLocalReferenceObjectExample();
 
-        public void increment() {
-            value++;
-        }
+    Thread thread1 =
+        new Thread(
+            () -> {
+              example.useLocalObject();
+            },
+            "Thread-1");
 
-        @Override
-        public String toString() {
-            return "LocalObject{" + "value=" + value + '}';
-        }
+    Thread thread2 =
+        new Thread(
+            () -> {
+              example.useLocalObject();
+            },
+            "Thread-2");
+
+    thread1.start();
+    thread2.start();
+  }
+
+  //    LocalObject localObject = new LocalObject();
+
+  public void useLocalObject() {
+    // 지역 객체 참조. 각 스레드는 이 객체의 독립된 인스턴스를 가짐.
+    LocalObject localObject = new LocalObject();
+
+    for (int i = 0; i < 5; i++) {
+      localObject.increment();
+      System.out.println(Thread.currentThread().getName() + " - " + localObject);
+      try {
+        Thread.sleep(50);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  class LocalObject {
+    private int value;
+
+    public void increment() {
+      value++;
     }
 
-//    LocalObject localObject = new LocalObject();
-
-    public void useLocalObject() {
-        // 지역 객체 참조. 각 스레드는 이 객체의 독립된 인스턴스를 가짐.
-        LocalObject localObject = new LocalObject();
-
-        for (int i = 0; i < 5; i++) {
-            localObject.increment();
-            System.out.println(Thread.currentThread().getName() + " - " + localObject);
-            try {
-                Thread.sleep(50);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+    @Override
+    public String toString() {
+      return "LocalObject{" + "value=" + value + '}';
     }
-
-    public static void main(String[] args) {
-        ThreadSafeLocalReferenceObjectExample example = new ThreadSafeLocalReferenceObjectExample();
-
-        Thread thread1 = new Thread(() -> {
-            example.useLocalObject();
-        }, "Thread-1");
-
-        Thread thread2 = new Thread(() -> {
-            example.useLocalObject();
-        }, "Thread-2");
-
-        thread1.start();
-        thread2.start();
-    }
+  }
 }
