ff528ee0293d0022fda946d9c89ae03d29772c77
KIM SOOBIN <xpsxm225@naver.com>
2024-12-01 14:36:53 +0900

    [Java-Concurrency] Mutex - 동기화 기법
    - for (int i = 0; i < 10_000_0000; i++) {
            sharedValue++;
          }은 임계영역으로, 공유 데이터인 sharedValue가 동시성 이슈에서 벗어나도록 설계해야 한다.
    - 이때 Mutex를 이용하여 동시성 문제를 해결한다.
    - acquired를 통해 lock을 획득하고 flag변수를 통해 다른 스레드가 임계영역에 접근하지 못하도록 한다.
    - 반드시 finally를 통해 lock을 해제(release)함으로써 다른 스레드가 임계영역을 수행할 수 있도록 보장한다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam01/SharedData.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam01/SharedData.java
@@ -1,23 +1,29 @@
 package io.concurrency.chapter06.exam01;
+
 public class SharedData {
-    private int sharedValue = 0;
-    private Mutex mutex;
-    public SharedData(Mutex mutex) {
-        this.mutex = mutex;
-    }
-    public void sum() {
-        try {
-            mutex.acquired(); // Lock 을 획득
+  private int sharedValue = 0;
+  private Mutex mutex;
 
-            for (int i = 0; i < 10_000_0000; i++) {
-                sharedValue++;
-            }
+  public SharedData(Mutex mutex) {
+    this.mutex = mutex;
+  }
 
-        } finally {
-            mutex.release(); // Lock 해제
-        }
+  public void sum() {
+    try {
+      mutex.acquired(); // Lock 을 획득
+
+      // 임계영역
+      for (int i = 0; i < 10_000_0000; i++) {
+        sharedValue++;
+      }
+
+    } finally {
+      // 반드시 finally 구문에서 lock을 해제한다.
+      mutex.release(); // Lock 해제
     }
-    public int getSum() {
-        return sharedValue;
-    }
+  }
+
+  public int getSum() {
+    return sharedValue;
+  }
 }
