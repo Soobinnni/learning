2c29af2c35b5b8a830c2a70ac0a14fa0519ed2d9
KIM SOOBIN <xpsxm225@naver.com>
2024-12-01 16:14:03 +0900

    [Java-Concurrency] java의 모니터 기능
    - 해당 코드는 그 기능 중 Mutex의 기능을 통해 상호 배제를 보장하고 있다.
    - synchronized 키워드를 선언하면 내부적으로 동시성 이슈를 해결한다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam03/MutualExclusionExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam03/MutualExclusionExample.java
@@ -1,29 +1,36 @@
 package io.concurrency.chapter06.exam03;
 
+// 이 예시는 자바의 모니터 기능 중 상호 배제(Mutex)를 보장하는 동기화 기법을 보여준다.
 public class MutualExclusionExample {
-    private int counter = 0;
+  private int counter = 0;
 
-    public synchronized void increment() {
-        counter++;
-        System.out.println("스레드: " + Thread.currentThread().getName() + ", 카운터 값: " + counter);
-    }
+  public static void main(String[] args) {
+    MutualExclusionExample example = new MutualExclusionExample();
 
-    public static void main(String[] args) {
-        MutualExclusionExample example = new MutualExclusionExample();
-
-        Thread thread1 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
+    Thread thread1 =
+        new Thread(
+            () -> {
+              for (int i = 0; i < 5; i++) {
                 example.increment();
-            }
-        });
+              }
+            });
 
-        Thread thread2 = new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
+    Thread thread2 =
+        new Thread(
+            () -> {
+              for (int i = 0; i < 5; i++) {
                 example.increment();
-            }
-        });
+              }
+            });
 
-        thread1.start();
-        thread2.start();
-    }
+    thread1.start();
+    thread2.start();
+  }
+
+  public synchronized void increment() {
+    // 클래스의 멤버 변수를 사용하고 있다.
+    // 즉 공유 자원을 연산하므로 synchronized 키워드를 통해 모니터 기능을 사용해야 한다.
+    counter++;
+    System.out.println("스레드: " + Thread.currentThread().getName() + ", 카운터 값: " + counter);
+  }
 }
