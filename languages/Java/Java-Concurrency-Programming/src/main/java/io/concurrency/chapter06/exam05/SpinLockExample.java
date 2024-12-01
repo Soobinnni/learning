ef9ae09f2684b87c695387bbea75c10511b0e2e2
KIM SOOBIN <xpsxm225@naver.com>
2024-12-01 16:56:32 +0900

    [Java-Concurrency] spin lock 동기화 기법
    - 블록킹 기반의 뮤텍스나 세마포어 방식과 달리 대기 상태(blocked)가 없다.
    - lock이 해제될 때까지 원자성이 보장된 test메서드의 반환값을 통해 무한루프를 돌 뿐이다.
    - 즉 자원 획득까지 반복적으로 검사를 수행한다.
    - 대기 상태가 없기 때문에 컨텍스트 스위칭이 일어나지 않으므로 해당 비용을 감소시키고 어느 면에서 빠른 공유 자원 접근이 가능하다.
    - 단, cpu 자원이 계속되어서 사용되기 때문에 대기 시간이 길거나 강한 경쟁 상태라면 사용하지 않는 것이 좋다.
    - 또한 싱글 스레드 환경에서 부적합하다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam05/SpinLockExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam05/SpinLockExample.java
@@ -3,39 +3,44 @@ package io.concurrency.chapter06.exam05;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SpinLockExample {
-    private AtomicBoolean lock = new AtomicBoolean(false);
+  private AtomicBoolean lock = new AtomicBoolean(false);
 
-    public void lock() {
-        while (!lock.compareAndSet(false, true)) ;
-    }
+  public static void main(String[] args) {
+    SpinLockExample spinLock = new SpinLockExample();
 
-    public void unlock() {
-        lock.set(false);
-    }
-
-    public static void main(String[] args) {
-        SpinLockExample spinLock = new SpinLockExample();
-
-        Runnable task = () -> {
-            spinLock.lock();
-            System.out.println(Thread.currentThread().getName() + " 락을 획득했습니다!");
+    Runnable task =
+        () -> {
+          spinLock.lock();
+          System.out.println(Thread.currentThread().getName() + " 락을 획득했습니다!");
+          try {
             try {
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            } finally {
-                System.out.println(Thread.currentThread().getName() + " 락을 해제합니다!");
-                spinLock.unlock();
-                System.out.println(Thread.currentThread().getName() + " 락을 해제했습니다!");
+              Thread.sleep(10);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
             }
+          } finally {
+            // unlock은 finally구문에서 해야 한다.
+            System.out.println(Thread.currentThread().getName() + " 락을 해제합니다!");
+            spinLock.unlock();
+            System.out.println(Thread.currentThread().getName() + " 락을 해제했습니다!");
+          }
         };
 
-        Thread thread1 = new Thread(task);
-        Thread thread2 = new Thread(task);
+    Thread thread1 = new Thread(task);
+    Thread thread2 = new Thread(task);
 
-        thread1.start();
-        thread2.start();
-    }
+    thread1.start();
+    thread2.start();
+  }
+
+  // 원자성을 보장하는 lock 검사 수행 구문, 언락 시때까지 스핀락
+  public void lock() {
+    while (!lock.compareAndSet(false, true))
+      ;
+  }
+
+  // 언락
+  public void unlock() {
+    lock.set(false);
+  }
 }
--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam05/SpinLockVsSynchronized.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter06/exam05/SpinLockVsSynchronized.java
@@ -3,66 +3,68 @@ package io.concurrency.chapter06.exam05;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SpinLockVsSynchronized {
-    private AtomicBoolean spinLock = new AtomicBoolean(false);
-    private final Object syncLock = new Object();
-    private int count = 0;
+  // THREAD_COUNT수가 높다면 컨텍스트 스위칭으로 스핀락의 성능이 더 높을 수 있다.
+  static final int THREAD_COUNT = 50;
+  final int ITERATIONS = 10_000_000;
+  private final Object syncLock = new Object();
+  private AtomicBoolean spinLock = new AtomicBoolean(false);
+  private int count = 0;
 
-    final static int THREAD_COUNT = 5;
-    final int ITERATIONS = 10_000_000;
+  public static void main(String[] args) throws InterruptedException {
 
-    public void useSpinLock() {
-        while (!spinLock.compareAndSet(false, true)) ;
-        for (int j = 0; j < ITERATIONS; j++) {
-            count++;
-        }
-        spinLock.set(false);
-    }
+    SpinLockVsSynchronized tester = new SpinLockVsSynchronized();
 
-    public void useSynchronized(){
-        synchronized (syncLock) {
-            for (int j = 0; j < ITERATIONS; j++) {
-                count++;
-            }
-        }
-    }
+    // synchronized 성능 테스트
+    Thread[] syncThreads = new Thread[THREAD_COUNT];
+    long start2 = System.currentTimeMillis();
 
-    public static void main(String[] args) throws InterruptedException {
-
-        SpinLockVsSynchronized tester = new SpinLockVsSynchronized();
-
-        // synchronized 성능 테스트
-        Thread[] syncThreads = new Thread[THREAD_COUNT];
-        long start2 = System.currentTimeMillis();
-
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            syncThreads[i] = new Thread(() -> {
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      syncThreads[i] =
+          new Thread(
+              () -> {
                 tester.useSynchronized();
-            });
-            syncThreads[i].start();
-        }
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            syncThreads[i].join();
-        }
-        long end2 = System.currentTimeMillis();
-        System.out.println("synchronized 시간: " + (end2 - start2));
-
-        // 스핀락 성능 테스트
-        Thread[] spinThreads = new Thread[THREAD_COUNT];
-        long start = System.currentTimeMillis();
-
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            spinThreads[i] = new Thread(() -> {
-                tester.useSpinLock();
-
-            });
-            spinThreads[i].start();
-        }
-        for (int i = 0; i < THREAD_COUNT; i++) {
-            spinThreads[i].join();
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("스핀락 시간: " + (end - start));
-
-
+              });
+      syncThreads[i].start();
     }
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      syncThreads[i].join();
+    }
+    long end2 = System.currentTimeMillis();
+    System.out.println("synchronized 시간: " + (end2 - start2));
+
+    // 스핀락 성능 테스트
+    Thread[] spinThreads = new Thread[THREAD_COUNT];
+    long start = System.currentTimeMillis();
+
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      spinThreads[i] =
+          new Thread(
+              () -> {
+                tester.useSpinLock();
+              });
+      spinThreads[i].start();
+    }
+    for (int i = 0; i < THREAD_COUNT; i++) {
+      spinThreads[i].join();
+    }
+    long end = System.currentTimeMillis();
+    System.out.println("스핀락 시간: " + (end - start));
+  }
+
+  public void useSpinLock() {
+    while (!spinLock.compareAndSet(false, true))
+      ;
+    for (int j = 0; j < ITERATIONS; j++) {
+      count++;
+    }
+    spinLock.set(false);
+  }
+
+  public void useSynchronized() {
+    synchronized (syncLock) {
+      for (int j = 0; j < ITERATIONS; j++) {
+        count++;
+      }
+    }
+  }
 }
