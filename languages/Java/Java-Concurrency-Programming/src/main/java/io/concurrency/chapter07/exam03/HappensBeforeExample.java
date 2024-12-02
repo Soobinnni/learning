dd7fe56f67e20ad3a3c72aa676feed01e24822e0
KIM SOOBIN <xpsxm225@naver.com>
2024-12-02 21:26:11 +0900

    [Java-Concurrency] volatile키워드의 가시성과 happens-before
    VolatileExample
        - 가시성에 대한 예시를 보여줌
        - volatile를 명시하지 않으면, 각자 캐시 메모리에서 running을 읽어오기 때문에 Thread 1이 영원히 종료되지 않음
    
    VolatileExample2
        - volatile키워드는 원래 가시성을 보장할 뿐 동시성을 보장하지 않음
        - 하지만 쓰기:읽기=N:1의 경우 둘 다 보장할 수 있음
        - 이 예시코드에선 위와 같은 상황이므로 둘에 안전한 코드임
    
    HappensBeforeExample
        - volatile키워드는 가시성뿐만 아니라 JMM의 스레드 간 작업 순서를 보장함
        - 데이터 레이스(Data Race)를 방지하고, 스레드 간의 일관성을 유지할 수 있음

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter07/exam03/HappensBeforeExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter07/exam03/HappensBeforeExample.java
@@ -1,40 +1,45 @@
 package io.concurrency.chapter07.exam03;
 
 public class HappensBeforeExample {
-    private int x = 0;
-//    private volatile boolean flag = false;
-    private boolean flag = false;
+  private int x = 0;
+  private volatile boolean flag = false;
 
-    public void writer() {
-        x = 42;
-        flag = true;
+  //    private boolean flag = false;
+
+  public static void main(String[] args) {
+    HappensBeforeExample example = new HappensBeforeExample();
+
+    Thread writerThread =
+        new Thread(
+            () -> {
+              example.writer();
+            });
+
+    Thread readerThread =
+        new Thread(
+            () -> {
+              example.reader();
+            });
+
+    writerThread.start();
+    readerThread.start();
+
+    try {
+      writerThread.join();
+      readerThread.join();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
+  }
 
-    public void reader() {
-        if (flag) {
-            System.out.println("result: " + x);
-        }
+  public void writer() {
+    x = 42;
+    flag = true;
+  }
+
+  public void reader() {
+    if (flag) {
+      System.out.println("result: " + x);
     }
-
-    public static void main(String[] args) {
-        HappensBeforeExample example = new HappensBeforeExample();
-
-        Thread writerThread = new Thread(() -> {
-            example.writer();
-        });
-
-        Thread readerThread = new Thread(() -> {
-            example.reader();
-        });
-
-        writerThread.start();
-        readerThread.start();
-
-        try {
-            writerThread.join();
-            readerThread.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
+  }
 }
