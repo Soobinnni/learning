9b038f1e3c5f128e2ec0e56381bc3a63a906c031
KIM SOOBIN <xpsxm225@naver.com>
2024-12-04 23:14:46 +0900

    [Java-Concurrency] 데드락의 경우의 수
    DeadlockOrderExample
        - 락 획득의 순서가 lock1->lock2, lock2->lock1의 상황이므로 획득하지 못하고 무한 대기 상태에 빠짐
        - 해결: NonDeadlockOrderExample
    
    DeadlockObjectsExample
        - 임계영역이 메서드 단위로 synchronized가 걸려 있기 때문에 데드락 상태가 됨
        - 해결1: NonDeadlockObjectsExample - 락 획득 순서 및 블록 synchronized로 해결
        - 해결2: NonDeadlockObjectsExample2 - 임계영역 설정을 블록단위로 세부 조절하여 해결
    
    DeadlockDynamicOrderExample
        - DeadlockOrderExample와 같은 상황
        - 해결: NonDeadlockDynamicOrderExample = if (System.identityHashCode(lockA) > System.identityHashCode(lockB))을 통해 순서를 보장하여 해결

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter07/exam04/NonDeadlockDynamicOrderExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter07/exam04/NonDeadlockDynamicOrderExample.java
@@ -1,36 +1,41 @@
 package io.concurrency.chapter07.exam04;
 
 public class NonDeadlockDynamicOrderExample {
-    private static final Object lock1 = new Object();
-    private static final Object lock2 = new Object();
+  private static final Object lock1 = new Object();
+  private static final Object lock2 = new Object();
 
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            methodWithLocks(lock1, lock2);
-        });
+  public static void main(String[] args) {
+    Thread thread1 =
+        new Thread(
+            () -> {
+              methodWithLocks(lock1, lock2);
+            });
 
-        Thread thread2 = new Thread(() -> {
-            methodWithLocks(lock2, lock1);
-        });
-        thread1.start();
-        thread2.start();
+    Thread thread2 =
+        new Thread(
+            () -> {
+              methodWithLocks(lock2, lock1);
+            });
+    thread1.start();
+    thread2.start();
+  }
+
+  private static void methodWithLocks(Object lockA, Object lockB) {
+    Object firstLock = lockA;
+    Object secondLock = lockB;
+
+    // 순서를 보장
+    if (System.identityHashCode(lockA) > System.identityHashCode(lockB)) {
+      firstLock = lockB;
+      secondLock = lockA;
     }
 
-    private static void methodWithLocks(Object lockA, Object lockB) {
-        Object firstLock = lockA;
-        Object secondLock = lockB;
+    synchronized (firstLock) {
+      System.out.println(Thread.currentThread().getName() + ": lockA 획득");
 
-        if (System.identityHashCode(lockA) > System.identityHashCode(lockB)) {
-            firstLock = lockB;
-            secondLock = lockA;
-        }
-
-        synchronized (firstLock) {
-            System.out.println(Thread.currentThread().getName() + ": lockA 획득");
-
-            synchronized (secondLock) {
-                System.out.println(Thread.currentThread().getName() + ": lockB 획득");
-            }
-        }
+      synchronized (secondLock) {
+        System.out.println(Thread.currentThread().getName() + ": lockB 획득");
+      }
     }
+  }
 }
--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter07/exam04/NonDeadlockObjectsExample2.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter07/exam04/NonDeadlockObjectsExample2.java
@@ -1,52 +1,53 @@
 package io.concurrency.chapter07.exam04;
 
 public class NonDeadlockObjectsExample2 {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        ResourceA resourceA = new ResourceA();
-        ResourceB resourceB = new ResourceB();
+    ResourceA resourceA = new ResourceA();
+    ResourceB resourceB = new ResourceB();
 
-        Thread thread1 = new Thread(() -> {
-            resourceA.methodA(resourceB);
-        });
+    Thread thread1 =
+        new Thread(
+            () -> {
+              resourceA.methodA(resourceB);
+            });
 
-        Thread thread2 = new Thread(() -> {
-            resourceB.methodB(resourceA);
-        });
+    Thread thread2 =
+        new Thread(
+            () -> {
+              resourceB.methodB(resourceA);
+            });
 
-        thread1.start();
-        thread2.start();
+    thread1.start();
+    thread2.start();
+  }
+
+  static class ResourceA {
+
+    public void methodA(ResourceB resourceB) { // 메서드 오픈
+      synchronized (this) {
+        System.out.println(Thread.currentThread().getName() + ": methodA 실행");
+      }
+      // A에 관한 lock을 해제한 뒤 다른 임계영역에 대한 lock을 획득하므로 데드락을 방지할 수 있음
+      resourceB.methodB2();
     }
 
-    static class ResourceA {
+    public synchronized void methodA2(ResourceB resourceB) {
+      System.out.println(Thread.currentThread().getName() + ": methodA2 실행");
+    }
+  }
 
-        public void methodA(ResourceB resourceB) { // 메서드 오픈
-            synchronized (this) {
-                System.out.println(Thread.currentThread().getName() + ": methodA 실행");
-            }
-            resourceB.methodB2();
-        }
+  static class ResourceB {
 
-        public synchronized void methodA2(ResourceB resourceB) {
-            System.out.println(Thread.currentThread().getName() + ": methodA2 실행");
-        }
+    public void methodB(ResourceA resourceA) { // 메서드 오픈
+      synchronized (this) {
+        System.out.println(Thread.currentThread().getName() + ": methodB 실행");
+      }
+      resourceA.methodA2(this);
     }
 
-    static class ResourceB {
-
-        public void methodB(ResourceA resourceA) { // 메서드 오픈
-            synchronized (this) {
-                System.out.println(Thread.currentThread().getName() + ": methodB 실행");
-            }
-            resourceA.methodA2(this);
-        }
-
-        public synchronized void methodB2() {
-            System.out.println(Thread.currentThread().getName() + ": methodB2 실행");
-        }
+    public synchronized void methodB2() {
+      System.out.println(Thread.currentThread().getName() + ": methodB2 실행");
     }
+  }
 }
-
-
-
-
