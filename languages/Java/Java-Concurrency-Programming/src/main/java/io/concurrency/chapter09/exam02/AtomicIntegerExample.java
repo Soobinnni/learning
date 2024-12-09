3b8cfcc41c96ff2bad1434ad3ccf273d95e7a094
KIM SOOBIN <xpsxm225@naver.com>
2024-12-09 21:19:26 +0900

    [Java-Concurrency] 단일 연산 변수
    synchronized와 단일 연산 변수(예: AtomicInteger와 CAS 연산) 의 주요 차이점은 락을 사용한 동기화와 CAS 연산을 통한 논블로킹 동기화다.
    
    synchronized
    - 락(Lock)을 사용하여 임계영역에 순차적으로 접근할 수 있도록 보장
    - 이를 통해 다른 스레드가 임계영역에 동시에 진입하지 않도록 막고, 동시성 문제를 해결
    - 순차적 접근을 보장하지만, 락을 획득하는 과정에서 블로킹이 발생할 수 있다.
    - 즉, 한 스레드가 락을 획득하면 다른 스레드는 대기해야 하므로 성능 저하가 발생
    - 임계영역을 차지한 스레드가 작업을 끝낼 때까지 다른 스레드는 대기하므로 컨텍스트 스위칭이 빈번히 발생할 수 있고, 락을 기다리는 시간 동안 스레드가 블로킹될 수 있다.
    
    CAS 연산 (단일 연산 변수)
    - CAS(Compare-And-Swap) 연산은 논블로킹 방식으로 동작
    - 즉, 연산을 완료할 때까지 다른 스레드가 개입하지 못하도록 보장하며, 스레드는 대기하지 않고, 성공적인 연산이 이루어질 때까지 재시도
    - 가시성과 원자성이 보장
        - Atomic 클래스의 메서드는 메인 메모리와의 동기화를 통해 다른 스레드가 변경한 값을 즉시 읽을 수 있도록 하며, 각 연산은 원자적으로 수행
    - 블로킹이 발생하지 않기 때문에 성능상 장점
        - 특히, 간단한 연산에서 synchronized보다 더 효율적
    - 하지만 CAS 연산도 재시도 루프를 사용하는데, 여러 스레드가 동시에 연산을 시도하면 CAS가 실패하고 재시도하는 경우가 많을 수 있고 이로 인해 성능 저하의 문제가 발생
    
    정리
    - 간단한 연산에서는 CAS 연산을 사용하는 단일 연산 클래스가 더 효율적일 수 있으며, 복잡한 연산이나 여러 연산을 보호할 필요가 있을 때는 synchronized가 유리할 수 있다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter09/exam02/AtomicIntegerExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter09/exam02/AtomicIntegerExample.java
@@ -4,30 +4,32 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class AtomicIntegerExample {
 
-    private static AtomicInteger counter = new AtomicInteger(0);
-    private static int NUM_THREADS = 5;
-    private static int NUM_INCREMENTS = 1000000;
+  private static AtomicInteger counter = new AtomicInteger(0);
+  private static int NUM_THREADS = 5;
+  private static int NUM_INCREMENTS = 1000000;
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        Thread[] threads = new Thread[NUM_THREADS];
+    Thread[] threads = new Thread[NUM_THREADS];
 
-        for (int i = 0; i <NUM_THREADS; i++) {
-            threads[i] = new Thread(()->{
+    for (int i = 0; i < NUM_THREADS; i++) {
+      threads[i] =
+          new Thread(
+              () -> {
                 for (int j = 0; j < NUM_INCREMENTS; j++) {
-                    counter.incrementAndGet();
-                    System.out.println("Counter: " + counter.get());
+                  counter.incrementAndGet();
+                  System.out.println("Counter: " + counter.get());
                 }
-            });
-            threads[i].start();
-        }
-        for(Thread thread:threads){
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        System.out.println("Final value: " + counter.get());
+              });
+      threads[i].start();
     }
+    for (Thread thread : threads) {
+      try {
+        thread.join();
+      } catch (InterruptedException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    System.out.println("Final value: " + counter.get());
+  }
 }
