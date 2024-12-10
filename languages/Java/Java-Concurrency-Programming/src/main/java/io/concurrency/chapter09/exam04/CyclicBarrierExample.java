36a3b220fc5f0da0b0f1e41286c136080fb5f296
KIM SOOBIN <xpsxm225@naver.com>
2024-12-10 19:16:58 +0900

    [Java-Concurrency] CyclicBarrier
    - 작업이 중요한 CountDownLatch와 달리 스레드(parties)가 중요
    - 모든 스레드가 동일한 지점에 도달할 때까지 기다리며 스레드 간 동기화가 중요하다.
    - 마지막으로 작업을 완수한 스레드가 장벽 액션을 수행하며, 장벽 액션이 장벽깨짐 없이 완수되었을 때 await이 깬다.
    - CyclicBarrier는 CountDownLatch와 달리 재사용이 가능하며, await메서드를 호출함으로써 가능하다.
    - 재사용시 마찬가지로 모든 스레드가 작업을 완수하면 마지막으로 장벽 액션을 수행하게 된다.

--- languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter09/exam04/CyclicBarrierExample.java
+++ languages/Java/Java-Concurrency-Programming/src/main/java/io/concurrency/chapter09/exam04/CyclicBarrierExample.java
@@ -4,61 +4,77 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class CyclicBarrierExample {
-    static int[] parallelSum = new int[2];
+  static int[] parallelSum = new int[2];
 
-    public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int numThreads = 2;
-        CyclicBarrier barrier = new CyclicBarrier(numThreads, new BarrierAction(parallelSum));
+  public static void main(String[] args) {
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int numThreads = 2;
+    CyclicBarrier barrier = new CyclicBarrier(numThreads, new BarrierAction(parallelSum));
 
-        for (int i = 0; i < numThreads; i++) {
-            new Thread(new Worker(i, numbers, barrier, parallelSum)).start();
-        }
+    for (int i = 0; i < numThreads; i++) {
+      new Thread(new Worker(i, numbers, barrier, parallelSum)).start();
     }
+  }
 }
 
 class BarrierAction implements Runnable {
-    private final int[] parallelSum;
+  private final int[] parallelSum;
 
-    public BarrierAction(int[] parallelSum) {
-        this.parallelSum = parallelSum;
-    }
+  public BarrierAction(int[] parallelSum) {
+    this.parallelSum = parallelSum;
+  }
 
-    public void run() {
-        int finalSum = 0;
-        for (int sum : parallelSum) {
-            finalSum += sum;
-        }
-        System.out.println("Final Sum: " + finalSum);
+  public void run() {
+    int finalSum = 0;
+    for (int sum : parallelSum) {
+      finalSum += sum;
     }
+    System.out.println("Final Sum: " + finalSum);
+  }
 }
 
 class Worker implements Runnable {
-    private final int id;
-    private final int[] numbers;
-    private final CyclicBarrier barrier;
-    private final int[] parallelSum;
+  private final int id;
+  private final int[] numbers;
+  private final CyclicBarrier barrier;
+  private final int[] parallelSum;
 
-    public Worker(int id, int[] numbers, CyclicBarrier barrier, int[] parallelSum) {
-        this.id = id;
-        this.numbers = numbers;
-        this.barrier = barrier;
-        this.parallelSum = parallelSum;
+  public Worker(int id, int[] numbers, CyclicBarrier barrier, int[] parallelSum) {
+    this.id = id;
+    this.numbers = numbers;
+    this.barrier = barrier;
+    this.parallelSum = parallelSum;
+  }
+
+  public void run() {
+    int start = id * (numbers.length / 2);
+    int end = (id + 1) * (numbers.length / 2);
+    int sum = 0;
+    for (int i = start; i < end; i++) {
+      sum += numbers[i];
     }
+    parallelSum[id] = sum;
 
-    public void run() {
-        int start = id * (numbers.length / 2);
-        int end = (id + 1) * (numbers.length / 2);
-        int sum = 0;
-        for (int i = start; i < end; i++) {
-            sum += numbers[i];
-        }
-        parallelSum[id] = sum;
+    try {
+      barrier.await();
+      // 장벽 액션 이후 await에서 풀려나며 스레드 개수가 2개이므로 두 번 출력됨
+      System.out.println("첫 번째 대기에서 풀려남");
 
-        try {
-            barrier.await();
-        } catch (InterruptedException | BrokenBarrierException e) {
-            e.printStackTrace();
-        }
+      barrier.await();
+      // 위 코드로 CyclicBarrier 재사용됨
+      // 장벽 액션 한 번 더 수행. 이후 await에서 풀려나며 스레드 개수가 2개이므로 두 번 출력됨
+      System.out.println("두 번째 대기에서 풀려남");
+
+      /*
+                Final Sum: 55
+               첫 번째 대기에서 풀려남
+               첫 번째 대기에서 풀려남
+               Final Sum: 55
+               두 번째 대기에서 풀려남
+               두 번째 대기에서 풀려남
+      */
+    } catch (InterruptedException | BrokenBarrierException e) {
+      e.printStackTrace();
     }
+  }
 }
